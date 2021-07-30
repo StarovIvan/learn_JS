f309e9613a8007e48eeefc96e595b36633dda620
Starov Ivan <pp24rsk@gmail.com>
2021-07-30 23:35:28 +0300

    Добавил переменные

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-const money = 1;
+const money = '100 рублей';
+const income = 40000;
+const addExpenses = 'string';
+const deposit = '400';
+const mission = 3;
+const period = 9; 
+
+alert(money);
+
+console.log(income);
