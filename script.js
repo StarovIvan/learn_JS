d2086dec332b377d6234e6ed949a4422f8958936
Starov Ivan <pp24rsk@gmail.com>
2021-08-07 17:45:22 +0300

    Использовал цикл и метод "charAt" для фильтрации чисел из массива

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
 'use strict';
 
-const number = 334523;
-let shortLine = '     Lorem ipsum dolor sit amet.      ';
-let longLine = '      Lorem ipsum dolor sit amet consectetur adipisicing elit.     ';
-
-const show = function (num){
-
-    if(num === Number(num)){
-        return 'Это число';
-    } else if(num === String(num)){
-        let width = num.trim().length;
-
-        if(width > 30){
-            return 'больше 30';
-        } else if(width < 30) {
-            return 'меньше 30';
-        }
+let arr = ['549', '23', '46952', '45743', '8765', '15234' , '25963'];
+let massiv = [];
+for(let i = 0; i < arr.length; i++){
+    let number1 = arr[i].charAt(0);
+    let number4 = arr[i].charAt(0);
+    if(number1 === '2' || number4 === '4'){
+        console.log(arr[i]);
     }
-};
-console.log(show(number));
-console.log(show(shortLine));
-console.log(show(longLine));
+}
+
+for(let i = 1; i <= 10; i++){
+    let dividers = `Делители числа ${i} является число 1 и ${i}`;
+    console.log(dividers);
+}
