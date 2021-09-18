724719596c027cd949bcebb5abc95ba0a938d108
Starov Ivan <pp24rsk@gmail.com>
2021-09-18 17:51:00 +0300

    переписал кнопку меню, модальное окно с помощью делегирования

--- timer/script/script.js
+++ timer/script/script.js
@@ -53,29 +53,35 @@ window.addEventListener('DOMContentLoaded', function(){
         }, 1000);
 
             
-        const showMenu = ()=> {
+        const toggleMenu = ()=> {
             const menuBtn = document.querySelector('.menu'),
                 menu = document.querySelector('menu'),
-                closeBtn = document.querySelector('.close-btn'),
-                menuItems = menu.querySelectorAll('ul>li');
+                closeBtn = document.querySelector('.close-btn');
 
 
-            
-            const menuHandler = ()=> {
-                menu.classList.toggle('active-menu');
+                menuBtn.addEventListener('click', (event)=> {
+                    let target = event.target;
+                        target = target.closest('.menu');
+                    if(target){
+                        menu.classList.add('active-menu');
+                    }    
+                });
 
-            };
+                menu.addEventListener('click', (event)=> {
+                    let target = event.target;
+                    if(target === closeBtn){
+                        menu.classList.remove('active-menu');
+                    } else {
+                        target = target.closest('a');
+                        if(target){
+                            menu.classList.remove('active-menu');
+                        }
+                    }
 
-                menuBtn.addEventListener('click', menuHandler);
-
-                closeBtn.addEventListener('click', menuHandler);
-
-                menuItems.forEach((elem)=> {
-                    elem.addEventListener('click', menuHandler);
                 });
                 
         };
-        showMenu();
+        toggleMenu();
 
         const popupContent = document.querySelector('.popup-content');
         popupContent.style.top = '-62%';
@@ -113,6 +119,49 @@ window.addEventListener('DOMContentLoaded', function(){
                 popupClose.addEventListener('click', ()=> popup.style.display = 'none');
                 popupClose.addEventListener('click', ()=> popupContent.style.top = '-62%');
 
+                popup.addEventListener('click', (event)=> {
+                    let target = event.target;
+                    if(target.classList.contains('popup-close')){
+                        popup.style.display = 'none';
+                    } else {
+                        target = target.closest('.popup-content');
+                        if(!target){
+                            popup.style.display = 'none';
+                        }
+                    }
+                });
+
         };
         showBtn();
+
+        const tabs = ()=> {
+            const tabHeader = document.querySelector('.service-header'),
+            tab = tabHeader.querySelectorAll('.service-header-tab'),
+            serviceTab = document.querySelectorAll('.service-tab');
+
+            const toggleTabContent = (index)=> {
+                for(let i = 0; i < serviceTab.length; i++){
+                    if(index === i){
+                        tab[i].classList.add('active');
+                        serviceTab[i].classList.remove('d-none');
+                    } else {
+                        tab[i].classList.remove('active');
+                        serviceTab[i].classList.add('d-none');
+                    }
+                }
+            };
+
+            tabHeader.addEventListener('click', (event)=> {
+                let target = event.target;
+                target = target.closest('.service-header-tab');
+                if(target){
+                    tab.forEach((item, index)=> {
+                        if(item === target){
+                            toggleTabContent(index);
+                        }
+                    });
+                }
+            });
+        };
+        tabs();
 });
